c4ffb212e94f8d7eaec8eb6fbefcc7c8a913ac04
hyunjun <homelus@daum.net>
2019-03-13 14:02:06 +0900

    설정 파일 추가

--- src/test/java/jun/spring/ch1/meta/BeanScanningTest.java
+++ src/test/java/jun/spring/ch1/meta/BeanScanningTest.java
@@ -4,6 +4,7 @@ import jun.spring.ch1.annotation.AnnotatedHello;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -12,11 +13,21 @@ import static org.hamcrest.core.IsNull.notNullValue;
 public class BeanScanningTest {
 
     @Test
-    public void simpleBeanScanning() {
+    public void 기본_빈_스캐닝() {
         ApplicationContext ctx = new AnnotationConfigApplicationContext("jun.spring.ch1.annotation"); // 빈스캐닝 패키지 지정
         AnnotatedHello hello = ctx.getBean("annotatedHello", AnnotatedHello.class); // 자동등록 빈 아이디는 첫 글자를 소문자로 바꿔서 사용
 
         assertThat(hello, is(notNullValue()));
     }
+
+    @Test
+    public void XML을_이용한_빈_스캐너_등록() {
+        ApplicationContext ctx = new GenericXmlApplicationContext("meta/beanScannerWithXML.xml");
+        AnnotatedHello hello = ctx.getBean("annotatedHello", AnnotatedHello.class);
+
+        assertThat(hello, is(notNullValue()));
+    }
+
+
 
 }
