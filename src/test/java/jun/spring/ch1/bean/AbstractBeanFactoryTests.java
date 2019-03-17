01e2c82925ce00104a08f8dea2a666df05b6e871
hyunjun <homelus@daum.net>
2019-03-17 22:11:57 +0900

    1. 1-2 정리 마무리

--- src/test/java/jun/spring/ch1/bean/AbstractBeanFactoryTests.java
+++ src/test/java/jun/spring/ch1/bean/AbstractBeanFactoryTests.java
@@ -1,7 +1,9 @@
 package jun.spring.ch1.bean;
 
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+
 public class AbstractBeanFactoryTests {
 
-
+    PropertyPlaceholderConfigurer
 
 }
